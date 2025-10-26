9390be1496bdcc1172a5d7233be96ed0531a0fa4
Ahm4dd <ahmed.emad.edu@gmail.com>
2025-10-26 16:11:53 +0300

    feat: added a function to clean input with tests

---
+++ src/tests/repl.test.ts
@@ -0,0 +1,23 @@
+import { cleanInput } from "./../repl";
+import { describe, expect, test} from "vitest";
+
+describe.each([
+    {
+        input: "    hello  world     ",
+        expected: ["hello", "world"]
+    },
+
+    {
+        input: "    WHAT    ARE  YOU    ",
+        expected: ["what", "are", "you"]
+    }
+])("cleanInput($input)", ({ input, expected }) => {
+    test(`Expected: ${expected}`, () => {
+        let actual = cleanInput(input);
+        expect(actual).toHaveLength(expected.length);
+
+        for (const i in expected) {
+            expect(actual[i]).toBe(expected[i]);
+        }
+    });
+});
